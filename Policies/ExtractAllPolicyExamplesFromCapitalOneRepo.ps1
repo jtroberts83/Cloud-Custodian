fe138d8f8907a31a19fc9f97e4433c7a87a56ecb
Jamison Roberts <jtroberts83@users.noreply.github.com>
2017-07-20 23:02:59 -0500

    Adding Powershell script which extracts examples
    This crawls through all the python scripts in the CapitalOne Cloud Custodian repo which you must have cloned down locally.  It then saves all the examples in a text file C:\temp\PolicyExamples.txt that you can search through etc from there.

---
+++ Policies/ExtractAllPolicyExamplesFromCapitalOneRepo.ps1
@@ -0,0 +1,48 @@
+﻿## put the folder location of your local copy of the Cloud Custodian repo here    
+$PathToLocalCopyOfCloudCustodianGitHubRepo = "C:\some-folder-location\cloud-custodian\" 
+##    Ex:   $PathToLocalCopyOfCloudCustodianGitHubRepo = "C:\GitHub\Cloud-Custodian\"  Make sure you point to the root of that repo folder
+
+# Set initial counter
+$PolicyCount = 0
+
+#Remove old PolicyExample.txt file if it exists
+if(Test-Path -Path "C:\temp\PolicyExamples.txt")
+{
+    Remove-Item -Path "C:\temp\PolicyExamples.txt"       
+}
+
+
+
+# Get the list of files in the Cloud Custodian repo recursively
+Get-ChildItem "$PathToLocalCopyOfCloudCustodianGitHubRepo" -Filter *.py -Recurse | 
+Foreach-Object {
+    #Reads in the .py file
+    $PolicyFile = Get-Content $_.FullName -Raw
+    $StartExampleIndex = 0
+    $EndExampleIndex = 0
+
+    #As long as there are more examples, keep iterating through
+    while(($StartExampleIndex -ge 0) -and ($PolicyFile))
+    {
+        # Most examples are encased in triple quotes """ so find the start and end position of those """ and extract the string inbetween them
+        $StartExampleIndex = $PolicyFile.IndexOf('"""',($StartExampleIndex + 1))
+        if($StartExampleIndex -ge 0)
+        {
+            $EndExampleIndex = $PolicyFile.IndexOf('"""', ($StartExampleIndex + 3))
+            $ExampleStringLength = $null
+            $ExampleStringLength = ($EndExampleIndex - $StartExampleIndex)
+            if($ExampleStringLength -gt 15)
+            {
+                $Example = $null
+                $Example = $PolicyFile.Substring($StartExampleIndex, ($ExampleStringLength + 3))
+                if(($Example.Contains("example")) -and ($Example.Contains("code-block: yaml")))
+                {
+                    $PolicyCount++
+                    Write-Host "`n $Example" -ForegroundColor Green
+                    # Write the examples to C:\temp\PolicyExamples.txt file
+                    Write-Output "`r`n`r`n $PolicyCount ##$Example " | Out-File "C:\temp\PolicyExamples.txt" -Append
+                }
+            }    
+        }
+    }
+ }
