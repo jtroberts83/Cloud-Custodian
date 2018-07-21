e9ff10e6d32ef362933c260a34794b228d30a188
Jamison Roberts <jtroberts83@users.noreply.github.com>
2018-07-21 12:36:23 -0500

    Update UserData.sh

--- c7n_org/UserData.sh
+++ c7n_org/UserData.sh
@@ -2,6 +2,7 @@
 
 export AWS_DEFAULT_REGION='us-east-1'
 
+##  SET YOUR S3 BUCKET NAME HERE WHICH HOSTS YOUR C7N-ORG CONFIG AND POLICIES
 RESOURCE_BUCKET='YOUR-S3-BUCKET-NAME'
 
 ## Install the needed components like git, dos2unix, gcc, pip, boto3, virtualenv
@@ -14,6 +15,7 @@ python get-pip.py --user
 pip install boto3
 pip install virtualenv
 cd /root
+
 
 ## Downloads latest build of Custodian and installs it along with c7n-org tool for multi-account policy runs
 git clone https://github.com/capitalone/cloud-custodian.git
@@ -30,7 +32,6 @@ mkdir /root/c7n_org_policies/regional
 mkdir /root/c7n_org_policies/global
 
 
-
 ## Downloads all the policy files from S3
 ## (I have the policies broken out by poll mode, lambda mode, and global (for services that only reside in us-east-1)
 aws s3 sync s3://$RESOURCE_BUCKET/policies/regional-poll/ /root/c7n_org_policies/regional/
@@ -45,7 +46,7 @@ aws s3 cp s3://$RESOURCE_BUCKET/C7n-Org-Config-Global.yaml /root/config-Global.y
 
 ## This part will run each policy through dos2unix to fix any odd formatting from windows and
 ## Then all regional policies are combined into 1 large policy doc that allows c7n-org to multithread WAY faster!
-cd /opt/c7n_org_policies/regional/
+cd /root/c7n_org_policies/regional/
 dos2unix *.yaml
 cat *.yaml > /tmp/allregional.yaml
 sed -i -e 's/policies://g' /tmp/allregional.yaml
@@ -54,7 +55,7 @@ echo 'policies:' | cat - /tmp/allregional.yaml > temp && mv temp /tmp/allregiona
 
 ## This part will run each policy through dos2unix to fix any odd formatting from windows and
 ## Then all global policies are combined into 1 large policy doc that allows c7n-org to multithread WAY faster!
-cd /opt/c7n_org_policies/global/
+cd /root/c7n_org_policies/global/
 dos2unix *.yaml
 cat *.yaml > /tmp/allglobal.yaml
 sed -i -e 's/policies://g' /tmp/allglobal.yaml
@@ -81,12 +82,33 @@ aws configure set aws_secret_access_key $aws_secret_access_key
 aws configure set aws_session_token $aws_session_token
 
 
-
+## Does a count of how many accounts you have listed in your c7n-org accounts config.yaml file
 NumberOfAccounts="$(grep -c 'name' /root/config-Regional.yaml)"
 touch /root/start-$NumberOfAccounts.txt
 aws s3 cp /root/start-$NumberOfAccounts.txt s3://$RESOURCE_BUCKET/start-$NumberOfAccounts.txt
 
 
+## Send txt message notification to cell phone indicating it is starting it's run
+aws sns publish --phone-number "1YOUR-Cell-Phone#" --message "Executing Custodian Scan Now"
 
-aws sns publish --phone-number "1YOUR-Cell-Phone#" --message "New Custodian Server Ready"
 
+for policyname in "${policyfiles[@]}"; do
+  echo "Running custodian with policy file $policyname"
+	POLICY_LOG_PATH=$policyname.txt
+  
+	## If its a regional policy run it against the multipls regions
+	if [[ $policyname == *"regional"* ]]; then
+		   c7n-org run -c /root/config-Regional.yaml -u /tmp/allregional.yaml -s /root/org-log --metrics --region us-east-1 --region eu-west-1 |& tee -a $POLICY_PATH                
+		   ObjectName="${policyname:1}"
+		   aws s3 cp $POLICY_PATH s3://$RESOURCE_BUCKET/$ObjectName
+		fi
+	else ## If its a global policy run it against us-east-1 only
+		c7n-org run -c /root/config-Global.yaml -u /tmp/allglobal.yaml -s /root/org-log --metrics --region us-east-1 |& tee -a $POLICY_PATH
+		ObjectName="${policyname:1}"
+		aws s3 cp $POLICY_PATH s3://$RESOURCE_BUCKET/$ObjectName
+	fi
+done
+
+
+touch /root/end.txt
+aws s3 cp /root/end.txt s3://$RESOURCE_BUCKET/end.txt
